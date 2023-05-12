517400c9690463fc469c2630a65a9da63df9e109
alcxz <masteral772@gmail.com>
2023-05-12 16:55:45 -0400

    After class May 12

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -3,24 +3,28 @@
 
 // }
 
-let firstDay = "Sunday";
-let days = 31;
+// let firstDay = "Sunday";
+// let days = 31;
 
-document.write("<table>");
+// document.write("<div id='calendarContainer'>");
+// document.write("<table>");
 
-if (firstDay == "Sunday") {
-    for (let i = 1; i <= days; i++) {
-        if (i % 7 == 1) {
-            document.write("<tr>");
-        }
+// if (firstDay == "Sunday") {
+//     for (let i = 1; i <= days; i++) {
+//         if (i % 7 == 1) {
+//             document.write("<tr>");
+//         }
 
-        document.write("<td>" + i + "</td>");
+//         document.write("<td>" + i + "</td>");
 
-        if (i % 7 == 0) {
-            document.write("</tr>");
-        } else if (i == days) {
-            document.write("</tr>");
-        }
+//         if (i % 7 == 0) {
+//             document.write("</tr>");
+//         } else if (i == days) {
+//             document.write("</tr>");
+//         }
 
-    }
-}
+//     }
+// }
+
+// document.write("</table>");
+// document.write("</div>");
