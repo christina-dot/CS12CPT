910e52a3944fc45ce687360e2eb61065ad922be7
alcxz <masteral772@gmail.com>
2023-05-14 15:36:34 -0400

    May 14 calendar update

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -28,3 +28,65 @@
 
 // document.write("</table>");
 // document.write("</div>");
+
+let startDay = 5;
+let days = 31;
+
+// let targetID = 1;
+
+// let targetCell = document.querySelector("#one" + targetID);
+// targetCell.innerHTML = "2013";
+
+let targetCell;
+let startPrinting = false;
+let dayToPrint = 1;
+
+for (let i = 1; i <= 6; i++) {
+    if (i == 1) {
+        for (let j = 1; j <= 7; j++) {
+            if (j == startDay) {
+                startPrinting = true;
+            }
+
+            if (startPrinting == true) {
+                targetCell = document.querySelector("#c" + i + j);
+                targetCell.innerHTML = dayToPrint;
+                dayToPrint += 1;
+            }
+        }
+    } else if (i == 5) {
+        for (let j = 1; j <= 7; j++) {
+            if (startPrinting == true) {
+                targetCell = document.querySelector("#c" + i + j);
+                targetCell.innerHTML = dayToPrint;
+            }
+            
+            if (dayToPrint == days) {
+                startPrinting = false;
+            } else {
+                dayToPrint += 1;
+            }
+        }
+    } else if (i == 6) {
+        for (let j = 1; j <= 7; j++) {
+            if (startPrinting == true) {
+                targetCell = document.querySelector("#c" + i + j);
+                targetCell.innerHTML = dayToPrint;
+            }
+
+            if (dayToPrint == days) {
+                startPrinting = false;
+            } else {
+                dayToPrint += 1;
+            }
+        }
+    } else {
+        for (let j = 1; j <= 7; j++) {
+            if (startPrinting == true) {
+                targetCell = document.querySelector("#c" + i + j);
+                targetCell.innerHTML = dayToPrint;
+                dayToPrint += 1;
+            }
+        }
+    }
+}
